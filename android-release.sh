9527a6f9bc6cea6c03aa7bb97de613d8057be9e2
Richard Shi <kmshi_array@yahoo.com>
2017-10-31 23:14:13 +0800

    add back ionic live update

--- android-release.sh
+++ android-release.sh
@@ -5,4 +5,5 @@ ionic cordova build --release --prod android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android-release-key.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk dtrcloud
 
 #zip align
+rm -f ./platforms/android/build/outputs/apk/android-release-signed.apk
 /Users/Shared/android/android-sdk/build-tools/23.0.1/zipalign -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk ./platforms/android/build/outputs/apk/android-release-signed.apk
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+declare var require: any;
+var myPackage = require('../../package.json');
+
 declare var window: any;
 var api_key = 's7dXctRdiXVRtc8PF2PKWjUk';
 
@@ -28,6 +31,8 @@ export class MyApp {
           position: 'top'
         }).present();
       }
+
+      showToast({version:myPackage.version});
 
       if (window.baiduPush) {
         window.baiduPush.onMessage(result => showToast({msg:result}), error => showToast(error));
