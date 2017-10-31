73db068dc4c995738e1a790598e7261b020a8890
Richard Shi <kmshi_array@yahoo.com>
2017-10-31 15:08:58 +0800

    Use baidu push and commit resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, ToastController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+
+declare var window: any;
+var api_key = 's7dXctRdiXVRtc8PF2PKWjUk';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,12 +14,31 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, toastCtrl: ToastController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      function showToast(msg){
+        toastCtrl.create({
+          message: JSON.stringify(msg),
+          duration: 3000,
+          position: 'top'
+        }).present();
+      }
+
+      if (window.baiduPush) {
+        window.baiduPush.onMessage(result => showToast({msg:result}), error => showToast(error));
+
+        window.baiduPush.onNotificationClicked(result => showToast(result), error => showToast(error));
+
+        window.baiduPush.onNotificationArrived(result => showToast(result), error => showToast(error));
+
+        window.baiduPush.startWork(api_key, result => {}, error => showToast(error));
+      }      
+
     });
   }
 }
