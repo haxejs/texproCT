b6ef1c327ae93b9086ac4bfe146f6ed690c68376
Richard Shi <kmshi_array@yahoo.com>
2017-11-02 21:01:08 +0800

    improve shell script

--- starthotcodepushdev.sh
+++ starthotcodepushdev.sh
@@ -1,5 +1,13 @@
+#build ionic source code
+ionic build
+
 #local hot code push dev
-./node_modules/.bin/cordova-hcp server
+./node_modules/.bin/cordova-hcp server &
+
+sleep 20
+
+#build and install binary app 
+cordova run android
 
 #debug
-#adb logcat chromium:D SystemWebViewClient:D *:S
+adb logcat chromium:D SystemWebViewClient:D *:S
