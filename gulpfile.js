3f84c708bdfeb9ba4e78215e5997a0274c9f424f
Richard Shi <kmshi_array@yahoo.com>
2017-11-03 10:47:56 +0800

    add: gulp liveupdate

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-//var leancloudInstaller = require('./plugins/cordova-plugin-leancloud/lpush-installer');
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+var chcpContext = require('./node_modules/cordova-hot-code-push-cli/dist/context.js');
+var build = require('./node_modules/cordova-hot-code-push-cli/dist/build.js');
 
-gulp.task('lpush-install', function(done){
-    //leancloudInstaller(__dirname, done);
+gulp.task('liveupdate', function(){
+    return watch(['www/assets/','www/build/'],function(vinyl){
+    	var context = chcpContext.context({});
+    	build.execute(context);
+    });
 });
