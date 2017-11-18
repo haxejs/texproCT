c2d36cc070e92996831b0ba4a3ac10b731623dad
Richard Shi <kmshi_array@yahoo.com>
2017-11-18 23:23:03 +0800

    code contacts page

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var require: any;
+var allOffices = require('./offices.json');
+
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+
+  private offices = allOffices;
 
   constructor(public navCtrl: NavController) {
   }
